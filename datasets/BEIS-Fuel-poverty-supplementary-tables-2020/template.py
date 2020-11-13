d28dba212f0315112216667a95ef9c7d389a0a66
mikeAdamss <mikelovesbooks@gmail.com>
2020-11-13 12:28:57 +0000

    local pipeline tweaks

--- datasets/BEIS-Fuel-poverty-supplementary-tables-2020/template.py
+++ datasets/BEIS-Fuel-poverty-supplementary-tables-2020/template.py
@@ -66,14 +66,14 @@ def generate_codelist_from_template(url, title, label, path_id):
       {{
         "virtual": true,
         "propertyUrl": "skos:inScheme",
-        "valueUrl": "{path_id}/concept/{pathified_label}"
+        "valueUrl": "{path_id}/concept-scheme/{pathified_label}"
       }}
     ],
     "primaryKey": "notation",
     "aboutUrl": "{path_id}/concept/{pathified_label}/{{notation}}"
   }},
   "prov:hadDerivation": {{
-    "@id": "{path_id}/scheme/{pathified_label}",
+    "@id": "{path_id}/concept-scheme/{pathified_label}",
     "@type": "skos:ConceptScheme",
     "rdfs:label": "{label}"
   }}
