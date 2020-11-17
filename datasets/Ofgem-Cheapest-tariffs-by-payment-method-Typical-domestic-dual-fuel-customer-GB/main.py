29342706d99a8e18cb909d54bfd104ad5881e40a
mikeAdamss <mikelovesbooks@gmail.com>
2020-11-17 16:40:17 +0000

    output csv

--- datasets/Ofgem-Cheapest-tariffs-by-payment-method-Typical-domestic-dual-fuel-customer-GB/main.py
+++ datasets/Ofgem-Cheapest-tariffs-by-payment-method-Typical-domestic-dual-fuel-customer-GB/main.py
@@ -57,11 +57,13 @@ trace.Value("Removed commas and whitespaces from Values")
 trace.store(title, df_final)
 trace.render("spec_v1.html")
 
+csvName = "{}.csv".format(pathify(title))
+df_final.to_csv("./out/{}".format(csvName))
+
 out = Path('out')
 out.mkdir(exist_ok=True)
 
 coldef = json.load(open('info.json'))
-csvName = "{}.csv".format(pathify(title))
 csvw_transform = CSVWMapping()
 csvw_transform.set_csv(out / csvName)
 csvw_transform.set_mapping(coldef)
