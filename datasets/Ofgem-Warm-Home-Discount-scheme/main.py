886740799c37b8ce89721cec9203038a32419100
RedWalters <jonathan.j.walters@gmail.com>
2020-12-14 14:10:43 +0000

    testing a name change

--- datasets/Ofgem-Warm-Home-Discount-scheme/main.py
+++ datasets/Ofgem-Warm-Home-Discount-scheme/main.py
@@ -343,7 +343,7 @@ We have gathered this information from obligated suppliers for information purpo
 We update this chart on an annual basis.
 """
 
-cubes.add_cube(scraper, df, 'nationexpenditure', info_json_dict=info_json)
+cubes.add_cube(scraper, df, 'ofgem-warm-home-discount-scheme', info_json_dict=info_json)
 trace.store('nationexpenditure', df)
 
 df
