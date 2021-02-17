af8fd2c42b2c2f1daaa8fd52da9dec234534b250
RedWalters <jonathan.j.walters@gmail.com>
2021-02-17 11:54:18 +0000

    updating to include the new updated publication which has time series, however, having to exclude pre-oct-2019 data from the 02 tab as it doesnt seem to reconcile with the rest of the data, have sent message to BAs to look into

--- datasets/DfT-Electric-vehicle-charging-device-statistics-Month-Year-data-tables/main.py
+++ datasets/DfT-Electric-vehicle-charging-device-statistics-Month-Year-data-tables/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[122]:
+# In[329]:
 
 
 
@@ -21,28 +21,29 @@ cubes = Cubes("info.json")
 info = json.load(open('info.json'))
 
 
-# In[123]:
+# In[330]:
 
 
-scraper = Scraper("https://www.gov.uk/government/statistics/electric-vehicle-charging-device-statistics-october-2020")
+scraper = Scraper("https://www.gov.uk/government/statistics/electric-vehicle-charging-device-statistics-january-2021")
+#Collections landing page doesnt pick up any distributions, will need to update the scraper to pick up new releases without manual input
 scraper
 
 
-# In[124]:
+# In[331]:
 
 
 for i in scraper.distributions:
     display(i)
 
 
-# In[125]:
+# In[332]:
 
 
 tabs = [x for x in scraper.distributions[1].as_databaker() if "Info" not in x.name] #
 tabs
 
 
-# In[126]:
+# In[333]:
 
 
 tidied_sheets = {}
@@ -59,22 +60,32 @@ for tab in tabs:
 
         LA = pivot.shift(0, 6).fill(DOWN).is_not_blank() - remove
 
-        measure = pivot.shift(2, 6).expand(RIGHT).is_not_blank()
+        period = pivot.shift(2,6).expand(RIGHT).is_not_blank()
+
+        measure = pivot.shift(2, 7).expand(RIGHT).is_not_blank()
 
         observations = LA.shift(2, 0).expand(RIGHT).is_not_blank() - remove
 
+        if tab.name in ['EVCD_01a']:
+            chargeType = 'all speeds'
+        elif tab.name in ['EVCD_01b']:
+            chargeType = 'rapid'
+
         dimensions = [
-                HDimConst("Period", "2020.0"),
+                HDim(period, "Period", CLOSEST, LEFT),
                 HDim(LA, 'Area', DIRECTLY, LEFT),
-                HDim(measure, 'Measure Type', DIRECTLY, ABOVE)
+                HDim(measure, 'Measure Type', DIRECTLY, ABOVE),
+                HDimConst('Charge Type', chargeType)
         ]
 
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        savepreviewhtml(tidy_sheet, fname="Preview.html")
+        savepreviewhtml(tidy_sheet, fname = tab.name + "-Preview.html")
 
         tidied_sheets[tab.name] = tidy_sheet.topandas()
 
-    if '02' in tab.name:
+    if '02Exclude' in tab.name:
+
+        #data in this tab is either incorrect or has been a result in change in methodology, is being looked into
 
         print(tab.name)
 
@@ -84,35 +95,48 @@ for tab in tabs:
 
         year = pivot.shift(0, 6).fill(DOWN).is_not_blank() - remove
 
-        quarter = year.shift(RIGHT)
+        quarter = pivot.shift(1, 6).fill(DOWN).is_not_blank() - remove
 
         measure = pivot.shift(2, 6).expand(RIGHT).is_not_blank()
 
-        observations = year.shift(2, 0).expand(RIGHT).is_not_blank() - remove
+        observations = quarter.shift(RIGHT).expand(RIGHT).is_not_blank() - remove
 
         dimensions = [
-                HDim(year, 'Period', DIRECTLY, LEFT),
+                HDim(year, 'Period', CLOSEST, ABOVE),
                 HDim(quarter, 'Quarter', DIRECTLY, LEFT),
                 HDimConst('Area', 'K02000001'),
                 HDim(measure, 'Measure Type', DIRECTLY, ABOVE)
         ]
 
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        savepreviewhtml(tidy_sheet, fname="Preview.html")
+        savepreviewhtml(tidy_sheet, fname = tab.name + "-Preview.html")
 
         tidied_sheets[tab.name] = tidy_sheet.topandas()
 
 
-# In[127]:
+# In[334]:
 
 
-df = pd.concat(tidied_sheets.values()).fillna('NaN')
+df = pd.concat(tidied_sheets.values(), sort = True).fillna('NaN')
 
-df['Period'] = df.apply(lambda x: 'year/' + left(x['Period'], 4) if 'NaN' in x['Quarter'] else 'quarter/'+left(x['Period'], 4)+'-'+x['Quarter'], axis = 1)
+df['Quarter'] = df.apply(lambda x: mid(x['Period'], 5, 5) if '-' in x['Period'] else x['Quarter'], axis = 1)
+df['Period'] = df.apply(lambda x: left(x['Period'], 4), axis = 1)
+df = df.replace({'January': 'Q1',
+                 'April': 'Q2',
+                 'July': 'Q3',
+                 'October': 'Q4',
+                 '01-01' : 'Q1',
+                 '04-01' : 'Q2',
+                 '07-01' : 'Q3',
+                 '10-01' : 'Q4'}, regex=True)
+
+df['Period'] = df.apply(lambda x: 'quarter/' + x['Period'] + '-' + x['Quarter'], axis = 1)
 
 df['Measure Type'] = df.apply(lambda x: x['Measure Type'].replace("\n", " "), axis = 1)
+
+df['Measure Type'] = df.apply(lambda x: 'Rapid Devices ' + x['Measure Type'] if 'rapid' in x['Charge Type'] and '100' in x['Measure Type'] else ('Total Devices ' + x['Measure Type'] if 'all speeds' in x['Charge Type'] and '100' in x['Measure Type'] else x['Measure Type']), axis = 1)
+
 df['Unit'] = 'Electric Vehicle Charging Platform'
-#df['Charge Type'] = df.apply(lambda x: 'Rapid' if 'rapid' in x['Measure Type'].lower() else 'all', axis = 1)
 df['OBS'] = df.apply(lambda x: "{:.2f}".format(x['OBS']), axis = 1)
 
 df = df.rename(columns={'OBS' : 'Value', 'Area' : 'Region'})
@@ -136,7 +160,7 @@ df = df[['Period', 'Region', 'Value', 'Measure Type', 'Unit']]
 df
 
 
-# In[128]:
+# In[335]:
 
 
 scraper.dataset.family = 'edvp'
@@ -150,13 +174,13 @@ csvName = 'observations'
 cubes.add_cube(scraper, df.drop_duplicates(), csvName)
 
 
-# In[129]:
+# In[336]:
 
 
 cubes.output_all()
 
 
-# In[130]:
+# In[337]:
 
 
 from IPython.core.display import HTML
@@ -165,33 +189,5 @@ for col in df:
         df[col] = df[col].astype('category')
         display(HTML(f"<h2>{col}</h2>"))
         display(df[col].cat.categories)
-
-
-# In[131]:
-
-
-import pandas as pd
-df = pd.read_csv("out/observations.csv")
-df["all_dimensions_concatenated"] = ""
-for col in df.columns.values:
-    if col != "Value":
-        df["all_dimensions_concatenated"] = df["all_dimensions_concatenated"]+df[col].astype(str)
-found = []
-bad_combos = []
-for item in df["all_dimensions_concatenated"]:
-    if item not in found:
-        found.append(item)
-    else:
-        bad_combos.append(item)
-df = df[df["all_dimensions_concatenated"].map(lambda x: x in bad_combos)]
-drop_these_cols = []
-for col in df.columns.values:
-    if col != "all_dimensions_concatenated" and col != "Value":
-        drop_these_cols.append(col)
-for dtc in drop_these_cols:
-    df = df.drop(dtc, axis=1)
-df = df[["all_dimensions_concatenated", "Value"]]
-df = df.sort_values(by=['all_dimensions_concatenated'])
-df.to_csv("duplicates_with_values.csv", index=False)
-# Find duplicates
+e)
 
