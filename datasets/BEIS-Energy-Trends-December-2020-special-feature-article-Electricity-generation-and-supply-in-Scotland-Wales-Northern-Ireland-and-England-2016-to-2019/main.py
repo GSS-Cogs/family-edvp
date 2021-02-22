d2f40a8a871d60bed73c37b81897ae3b2620aa08
Osamede Asemota <osamedeasemota@C02D75L5MD6R.home>
2021-02-22 17:56:25 +0000

    Transform completed in line with the notes taken during the EDV dataset start-up meeting.

--- datasets/BEIS-Energy-Trends-December-2020-special-feature-article-Electricity-generation-and-supply-in-Scotland-Wales-Northern-Ireland-and-England-2016-to-2019/main.py
+++ datasets/BEIS-Energy-Trends-December-2020-special-feature-article-Electricity-generation-and-supply-in-Scotland-Wales-Northern-Ireland-and-England-2016-to-2019/main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -12,6 +11,8 @@
 #     language: python
 #     name: python3
 # ---
+
+# Electricity generation and supply in Scotland, Wales, Northern Ireland and England, 2016 to 2019
 
 import copy
 import json
@@ -85,19 +86,20 @@ for tab in tabs:
         trace.start(title, tab, columns, distribution.downloadURL)
 
         footnote = tab.excel_ref('A23').expand(DOWN)
+        
         generation = tab.excel_ref('A5').expand(DOWN).is_not_blank() - footnote
         region = tab.excel_ref('B4').expand(RIGHT).is_not_blank()
+        
+        trace.Period("Selected as the given years, with the blank cells filled in with the 'with_year_overrides' function")
         period = region.shift(UP) 
-#         period = tab.excel_ref('B3').expand(RIGHT).is_not_blank()
-    
-#         observations = generation.shift(RIGHT).is_not_blank()
+
         observations = tab.excel_ref('B5').expand(DOWN).expand(RIGHT).is_not_blank()
 
         dimensions = [
-#             HDim(period, 'Period', CLOSEST, LEFT),
             HDim(period, 'Period', DIRECTLY, ABOVE),
             HDim(region, 'Region', DIRECTLY, ABOVE),
             HDim(generation, 'Description of Generation & Supply', DIRECTLY, LEFT),
+            
             HDimConst('Measure Type', 'Electricity'),
             HDimConst('Unit', 'GWh')
         ]
@@ -116,7 +118,7 @@ for tab in tabs:
             'England' : 'K04000001'    
             }})
         
-        df['Period'] = df.Period.str.replace('\.0', '')
+        df['Period'] = df['Period'].astype(float).astype(int)
         df['OBS'] = df['OBS'].astype(int)
         df.rename(columns={'OBS' : 'Value'}, inplace=True)
         tidy = df[['Period', 'Region', 'Description of Generation & Supply', 'Measure Type', 'Unit', 'Value']]
@@ -127,9 +129,8 @@ for tab in tabs:
 
         cubes.add_cube(copy.deepcopy(scraper), tidy, scraper.dataset.title)
         
-    #
-    elif tab.name == tab_names[2]:
-        title = title2 + 
+    elif tab.name == 'Electricity generation by fuel':
+        title = title2 
         scraper.dataset.title = title
         scraper.dataset.comment = title
         scraper.dataset.description = title
@@ -137,39 +138,45 @@ for tab in tabs:
         columns = ['Period', 'Region', 'Generating Company', 'Fuel Type', 'Measure Type', 'Unit', 'Value']
         trace.start(title, tab, columns, distribution.downloadURL)
         
-        # # # Cells not required
         footnote = tab.excel_ref('A53').expand(DOWN).expand(RIGHT).is_not_blank()
+        
         within_which = tab.excel_ref('A').expand(DOWN).by_index([20, 27, 32, 41, 46])
         
         share_totalGen = tab.excel_ref('A40').expand(DOWN).expand(RIGHT) - footnote
         
         total_electricity = tab.excel_ref('B').expand(DOWN).by_index([15, 25, 39])
-        generators1 = tab.excel_ref('A5').expand(DOWN).is_not_blank()|total_electricity 
-        generators = generators1 - within_which - share_totalGen - footnote
+        
+        trace.Generating_Company('Selected as the electricity generating companies with the totals pulled over \
+        from cell B, and with the share of total generation(%) and the footnote removed')
+        generators = tab.excel_ref('A5').expand(DOWN).is_not_blank()|total_electricity 
+        generators = generators - within_which - share_totalGen - footnote
+        
+        trace.Fuel_Type('Selected as the fuel type with the share of total generation(%) and the footnote removed. \
+            The totals cell pulled over to generators are removed below')
         fuel = tab.excel_ref('B5').expand(DOWN).is_not_blank() - share_totalGen - footnote 
+        
         region = tab.excel_ref('C4').expand(RIGHT).is_not_blank()
-#         period = region.shift(UP) 
-        period = tab.excel_ref('C3').expand(RIGHT).is_not_blank()
-    
-#         observations = generation.shift(RIGHT).is_not_blank()
+        
+        trace.Period("Selected as the given years with the blank cells filled in with the 'with_year_overrides' function")
+        period = region.shift(UP) 
+
         observations = tab.excel_ref('C5').expand(DOWN).expand(RIGHT).is_not_blank() - share_totalGen - footnote
         
-        # # # Correcting the 'total' cells in fuel column
+        '''Correcting the 'total' cells in fuel column'''
         fuel_override = {}
         for cell in fuel:
             if cell in generators:
-#             if cell.value in ['TOTAL MPPs', 'TOTAL OTHER GENERATORS', 'TOTAL ALL GENERATING COMPANIES']:
                 fuel_override[cell.value] = 'all'
                 
-        dimensions = [
-            HDim(period, 'Period', CLOSEST, LEFT),
+        dimensions = [ 
+            HDim(period, 'Period', DIRECTLY, ABOVE),
             HDim(region, 'Region', DIRECTLY, ABOVE),
             HDim(generators, 'Generating Company', CLOSEST, ABOVE),
             HDim(fuel, 'Fuel Type', DIRECTLY, LEFT, cellvalueoverride = fuel_override),
             HDimConst('Measure Type', 'Electricity'),
             HDimConst('Unit', 'GWh')
         ]
-#         dimensions[0] = with_year_overrides(dimensions[0])
+        dimensions[0] = with_year_overrides(dimensions[0])
         
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
         trace.store('dataframe2', tidy_sheet.topandas())
@@ -184,24 +191,17 @@ for tab in tabs:
             'England' : 'K04000001'    
             }})
         
-#         # # # removing footnote caption from fuel type
-#         df['Fuel Type'] = df['Fuel Type'].str.replace(r'\(.*\)', ' ')
+        '''removing footnote caption from fuel type'''
+        df['Fuel Type'] = df['Fuel Type'].str.replace(r'\(.*\)', ' ')
         
-        df['Period'] = df.Period.str.replace('\.0', '')
+        df['Period'] = df['Period'].astype(float).astype(int)
         df['OBS'] = df['OBS'].astype(int)
         df.rename(columns={'OBS' : 'Value'}, inplace=True)
         
         tidy = df[['Period', 'Region', 'Generating Company', 'Fuel Type', 'Measure Type', 'Unit', 'Value']]
         for column in tidy:
-            if column in ('Generating Company', 'Fuel Type'):
+            if column in ('Generating Company', 'Fuel Type', 'Measure Type'):
                 tidy[column] = tidy[column].map(lambda x: pathify(x))
 
         cubes.add_cube(copy.deepcopy(scraper), tidy, scraper.dataset.title)
 cubes.output_all()  
-
-# +
-# cubes.add_cube(scraper, df, title)
-# cubes.output_all()
-# -
-
-
