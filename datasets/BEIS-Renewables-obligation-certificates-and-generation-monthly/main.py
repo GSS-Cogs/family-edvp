3279018a3ad435cce77a9498dfdb98962253ac33
santhosh thangavel <santhoshk.thangavel@gmail.com>
2021-02-23 16:07:56 +0000

    1.Stage - 1 transform completed 2.Post processing completed 3.Cubes class added 4.TransformTrace added 5.Footer is removed 6.Appropriate direction is used when defining dimensions 7.columns are patified

--- datasets/BEIS-Renewables-obligation-certificates-and-generation-monthly/main.py
+++ datasets/BEIS-Renewables-obligation-certificates-and-generation-monthly/main.py
@@ -39,12 +39,14 @@ for tab in tabs:
     print(tab.name)
     
     trace.start(datasetTitle, tab, columns, distribution.downloadURL)
+    
+    remove = tab.filter(contains_string("Data are sourced")).expand(RIGHT).expand(DOWN)
     cell = tab.excel_ref("A1")
     
     generation_type = cell.shift(1, 5).fill(DOWN).is_not_blank().is_not_whitespace()
     trace.Generation_Type("Defined from cell B6 below which is not blank")
 
-    roc_per_mwh = cell.shift(2, 5).fill(DOWN).is_not_blank().is_not_whitespace()
+    roc_per_mwh = cell.shift(2, 5).fill(DOWN)-remove
     trace.Roc_Per_Mwh("Defined from cell C6 below which is not blank")
     
     if tab.name == "Quarter":
@@ -66,19 +68,17 @@ for tab in tabs:
 
     techno_group = cell.shift(0, 5).fill(DOWN).is_not_blank().is_not_whitespace()-element
     
-    technology_group = techno_group - tab.excel_ref("A").filter(contains_string("Summary Technology Group"))
+    technology_group = techno_group - tab.excel_ref("A").filter(contains_string("Summary Technology Group")) - remove
     trace.Technology_Group("Defined from cell A8 which is not blank")
     
-    observations = cell.shift(3, 6).expand(RIGHT).expand(DOWN).is_not_whitespace()
-# ROCs and MWh needs to be removed from the observation which is getting caught in DATAMARKER.
-# true_observations = observations.filter(lambda x: type(x.value) != "ROCs" not in x.value)
+    observations = cell.shift(3, 6).expand(RIGHT).expand(DOWN).is_not_whitespace() - element.expand(RIGHT)
 
     if tab.name == "Quarter": 
     
         dimensions = [
             HDim(technology_group, "Technology Group", CLOSEST, ABOVE),
             HDim(generation_type, "Generation Type", CLOSEST, ABOVE),
-            HDim(roc_per_mwh, "Roc Per Mwh", CLOSEST, ABOVE),
+            HDim(roc_per_mwh, "Roc Per Mwh", DIRECTLY, LEFT, cellvalueoverride = {'' : 'all'}),
             HDim(period, "Period", CLOSEST, LEFT),
             HDim(element, "Element", CLOSEST, ABOVE),
             HDim(qtr, "Qtr", CLOSEST, LEFT)
@@ -89,7 +89,7 @@ for tab in tabs:
         dimensions = [
             HDim(technology_group, "Technology Group", CLOSEST, ABOVE),
             HDim(generation_type, "Generation Type", CLOSEST, ABOVE),
-            HDim(roc_per_mwh, "Roc Per Mwh", CLOSEST, ABOVE),
+            HDim(roc_per_mwh, "Roc Per Mwh", DIRECTLY, LEFT, cellvalueoverride = {'' : 'all'}),
             HDim(period, "Period", CLOSEST, LEFT),
             HDim(element, "Element", CLOSEST, ABOVE),
             HDim(month, "Month", CLOSEST, LEFT)
@@ -99,7 +99,7 @@ for tab in tabs:
         dimensions = [
             HDim(technology_group, "Technology Group", CLOSEST, ABOVE),
             HDim(generation_type, "Generation Type", CLOSEST, ABOVE),
-            HDim(roc_per_mwh, "Roc Per Mwh", CLOSEST, ABOVE),
+            HDim(roc_per_mwh, "Roc Per Mwh", DIRECTLY, LEFT, cellvalueoverride = {'' : 'all'}),
             HDim(period, "Period", CLOSEST, LEFT),
             HDim(element, "Element", CLOSEST, ABOVE),
 
@@ -111,12 +111,54 @@ for tab in tabs:
     trace.store("combined_dataframe", tidy_sheet.topandas())
 # -
 
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe")
+df = trace.combine_and_trace(datasetTitle, "combined_dataframe").fillna("NaN")
 df
 
-df["DATAMARKER"].unique()
 
-df["Element"].unique()
+def left(s, amount):
+    return s[:amount]
+def right(s, amount):
+    return s[-amount:]
+
+
+df['Period'] = df.apply(lambda x: 'quarter/' + left(x['Period'], 4) + '-Q' + left(x['Qtr'], 1) if 'NaN' not in x['Qtr'] else x['Period'], axis =1 )
+
+df = df.replace({'Month' : {'September' : '09',
+                            'October'   : '10',
+                            'November'  : '11',
+                            'December'  : '12',
+                            'January'   : '01',
+                            'February'  : '02',
+                            'March'     : '03',
+                            'April'     : '04',
+                            'May'       : '05',
+                            'June'      : '06',
+                            'July'      : '07',
+                            'August'    : '08',
+                            'October p' : '10'},
+                 'Technology Group' : {'Total' : 'All'}})
+
+df['Period'] = df.apply(lambda x: 'month/' + left(x['Period'], 4) + '-' + x['Month'] if 'NaN' not in x['Month'] else x['Period'], axis =1 )
+
+df['Period'] = df.apply(lambda x: 'financial-year/' + left(x['Period'], 4) if 'NaN' in x['Qtr'] and 'NaN' in x['Month'] else x['Period'], axis = 1)
+
+df = df.drop(columns=['Qtr', 'Month'])
+
+df = df.rename(columns={'OBS' : 'Value'})
+
+df = df[['Period', 'Technology Group', 'Generation Type', 'Roc Per Mwh', 'Value', 'Element']]
+
+COLUMNS_TO_NOT_PATHIFY = ['Period', 'Value', 'Roc Per Mwh']
+
+for col in df.columns.values.tolist():
+    if col in COLUMNS_TO_NOT_PATHIFY:
+        continue
+    try:
+        df[col] = df[col].apply(pathify)
+    except Exception as err:
+        raise Exception('Failed to pathify column "{}".'.format(col)) from err
+
+df
 
 cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
 cubes.output_all()
