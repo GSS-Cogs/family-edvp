88ad8ec56668588e12351335b271d69726115abc
RedWalters <jonathan.j.walters@gmail.com>
2021-03-16 13:43:11 +0000

    adding values to family codelist

--- datasets/BEIS-Energy-Trends-December-2020-special-feature-article-Electricity-generation-and-supply-in-Scotland-Wales-Northern-Ireland-and-England-2016-to-2019/main.py
+++ datasets/BEIS-Energy-Trends-December-2020-special-feature-article-Electricity-generation-and-supply-in-Scotland-Wales-Northern-Ireland-and-England-2016-to-2019/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[24]:
 
 
 # ---
@@ -220,20 +220,25 @@ for tab in tabs:
 
         tidy = tidy.replace({'Fuel' : {'total-mpps' : 'all',
                                        'total-all-generating-companies' : 'all',
-                                       'total-other-generators' : 'all'}})
+                                       'total-other-generators' : 'all'},
+                             'Generating Companies' : {
+                                        'total-other-generators' : 'other-generators',
+                                        'total-all-generating-companies' : 'all-generating-companies',
+                                        'major-power-producers-mpps' : 'major-power-producers',
+                                        'total-mpps' : 'major-power-producers'}})
 
         cubes.add_cube(copy.deepcopy(scraper), tidy, scraper.dataset.title)
 
 tidy
 
 
-# In[2]:
+# In[25]:
 
 
 cubes.output_all()
 
 
-# In[3]:
+# In[26]:
 
 
 
