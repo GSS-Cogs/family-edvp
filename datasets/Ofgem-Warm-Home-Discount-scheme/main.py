5837ed745f37c2a18ce16bcb6d5fd762ded20fa5
RedWalters <jonathan.j.walters@Gmail.com>
2021-08-12 11:56:52 +0100

    The method to which we originally gained the link to the CSV files is no longer working, so will have to use direct files for now

--- datasets/Ofgem-Warm-Home-Discount-scheme/main.py
+++ datasets/Ofgem-Warm-Home-Discount-scheme/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-# %%
 
-# %%
+# In[1]:
 
 
 import pandas as pd
@@ -20,10 +19,9 @@ def mid(s, offset, amount):
     return s[offset:offset+amount]
 
 
-# %%
+# In[2]:
 
 
-trace = TransformTrace()
 cubes = Cubes("info.json", job_name='')
 
 publisher = "The Office of Gas and Electricity Markets"
@@ -34,32 +32,29 @@ with open('info.json') as f:
 info_file_data["dataURL"] = "https://www.ofgem.gov.uk/node/112635/revisions/374173/csv?fake=.csv"
 with open('info.json', 'w') as f:
     json.dump(info_file_data, f, indent=2)
+
 scraper = Scraper(seed="info.json")
-title = "ofgem-112635-WarmHomeDiscount_Distributionofexpenditurebyyear".replace('-', ' ')
+title = "ofgem-WarmHomeDiscount_Distributionofexpenditurebyyear".replace('-', ' ')
 scraper.distributions[0].title = title
 scraper
 
 
-# %%
+# In[3]:
 
 
-link = scraper.distributions[0].downloadURL
-columns = ['Period', 'Scheme Year', 'Support Element', 'Value']
-trace.start(publisher, 'percentageexpenditure', columns, link)
-
-df = scraper.distributions[0].as_pandas()
+df = pd.read_csv('distribution-expenditure.csv')
 
 dimensions = list(df.columns) #%%list of columns
-dimensions = [col for col in dimensions if 'spending proportion' not in col.lower()] #%%list of the dimensions
+dimensions = [col for col in dimensions if 'category' not in col.lower()] #%%list of the dimensions
 
-df_new_shape = pd.melt(df, id_vars=["Spending proportion"])
+df_new_shape = pd.melt(df, id_vars=["Category"])
 df_new_shape = df_new_shape.rename(columns={"variable": "Support Element", "value": "Value"})
 
-df = df_new_shape[['Spending proportion', 'Support Element', 'Value']]
+df = df_new_shape[['Category', 'Support Element', 'Value']]
 
-df['Scheme Year'] = df.apply(lambda x: left(x['Spending proportion'].replace('Scheme ', ''), 6), axis = 1)
+df['Scheme Year'] = df.apply(lambda x: left(x['Category'].replace('Scheme ', ''), 6), axis = 1)
 
-df = df.replace({'Spending proportion' : {'Scheme Year 1 2011/12' : '1 April 2011 to 31 March 2012',
+df = df.replace({'Category' : {'Scheme Year 1 2011/12' : '1 April 2011 to 31 March 2012',
                              'Scheme Year 2 2012/13': '1 April 2012 to 31 March 2013',
                              'Scheme Year 3 2013/14': '1 April 2013 to 31 March 2014',
                              'Scheme Year 4 2014/15': '1 April 2014 to 31 March 2015',
@@ -69,11 +64,11 @@ df = df.replace({'Spending proportion' : {'Scheme Year 1 2011/12' : '1 April 201
                              'Scheme Year 8 2018/19': '15 August 2018 to 31 March 2019',
                              'Scheme Year 9 2019/20': '1 April 2019 to 31 March 2020'}})
 
-df['Period Lower'] = df.apply(lambda x: parse(x['Spending proportion'].split(' to ')[0]).date(), axis = 1)
-df['Period Higher'] = df.apply(lambda x: parse(x['Spending proportion'].split(' to ')[1]).date(), axis = 1)
+df['Period Lower'] = df.apply(lambda x: parse(x['Category'].split(' to ')[0]).date(), axis = 1)
+df['Period Higher'] = df.apply(lambda x: parse(x['Category'].split(' to ')[1]).date(), axis = 1)
 df['Period'] = df.apply(lambda x: 'gregorian-interval/'+ str(x['Period Lower']) + 'T00:00:00/P' + str((x['Period Higher'] - x['Period Lower']).days) + 'D', axis = 1)
 
-df['Value'] = df.apply(lambda x: x['Value'].replace('%', ''), axis = 1)
+df['Value'] = df.apply(lambda x: str(x['Value']).replace('%', ''), axis = 1)
 
 df = df[['Period', 'Scheme Year', 'Support Element', 'Value']]
 
@@ -87,22 +82,12 @@ for col in df.columns.values.tolist():
     except Exception as err:
         raise Exception('Failed to pathify column "{}".'.format(col)) from err
 
-mapping = {}
 with open("info.json") as f:
     info_json = json.load(f)
 
     info_json["transform"]["columns"]['Value']["unit"] = "http://gss-data.org.uk/def/concept/measurement-units/percentage"
     info_json["transform"]["columns"]['Value']["measure"] = "http://gss-data.org.uk/def/measure/expenditure"
     info_json["transform"]["columns"]['Value']["datatype"] = "double"
-
-print("Mapping for: ", 'percentageexpenditure')
-print(json.dumps(mapping, indent=2))
-print("\n")
-
-trace.Period("Values taken from 'Spending proportion' column")
-trace.Scheme_Year("Values taken from 'Spending proportion' column")
-trace.Support_Element("Values one of {}", dimensions)
-trace.Value("Values taken from {} columns", dimensions)
 
 scraper.dataset.title = 'Warm Home Discount Scheme: Distribution of expenditure by year'
 
@@ -122,35 +107,17 @@ We update this chart on an annual basis.
 df.head(10)
 
 
-# %%
+# In[4]:
 
 
-import os
-from urllib.parse import urljoin
+csvName = 'distributionexpenditure'
 
-csvName = 'percentageexpenditure.csv'
-out = Path('out')
-out.mkdir(exist_ok=True)
-df.drop_duplicates().to_csv(out / csvName, index = False)
-df.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
+scraper.dataset.family = 'energy'
 
-scraper.dataset.family = 'edvp'
-dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower() + "/percentageexpenditure"
-scraper.set_base_uri('http://gss-data.org.uk')
-scraper.set_dataset_id(dataset_path)
-
-csvw_transform = CSVWMapping()
-csvw_transform.set_csv(out / csvName)
-csvw_transform.set_mapping(info_json)
-csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
-csvw_transform.write(out / f'{csvName}-metadata.json')
-
-with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
+cubes.add_cube(scraper, df, csvName)
 
 
-# %%
-
+# In[5]:
 
 
 with open('info.json') as f:
@@ -164,36 +131,24 @@ scraper.distributions[0].title = title
 scraper
 
 
-# %%
+# In[6]:
 
 
-
-def sanitize_values(value):
-    try:
-        new_value = value.replace(' ', '').replace(',', '').strip()
-        return new_value
-    except:
-        return ''
-
-link = scraper.distributions[0].downloadURL
-columns = ['Period', 'Scheme Year', 'Supplier', 'Support Element', 'Value']
-trace.start(publisher, 'gbpexpenditure', columns, link)
-
-df = scraper.distributions[0].as_pandas()
+df = pd.read_csv('total-expenditure.csv')
 
 dimensions = list(df.columns) #%%list of columns
-dimensions = [col for col in dimensions if 'supplier' not in col.lower()] #%%list of the dimensions
+dimensions = [col for col in dimensions if 'category' not in col.lower()] #%%list of the dimensions
 
-df_new_shape = pd.melt(df, id_vars=["Supplier"])
+df_new_shape = pd.melt(df, id_vars=["Category"])
 df_new_shape = df_new_shape.rename(columns={"variable": "Support Element", "value": "Value"})
 
 df = df_new_shape.fillna('')
-df['Period'] = df.apply(lambda x: 'gregorian-interval/'+ str(parse('15 August 2018').date()) + 'T00:00:00/P' + str((parse('31 March 2019') - parse('15 August 2018')).days) + 'D', axis = 1)
-df['Scheme Year'] = 'year-8'
-df['Marker'] = df.apply(lambda x: 'not-applicable' if x['Value'] == '0' else '', axis = 1)
-df['Value'] = df.apply(lambda x: sanitize_values(x['Value']), axis = 1)
+df['Period'] = df.apply(lambda x: 'gregorian-interval/'+ str(parse('01 April 2019').date()) + 'T00:00:00/P' + str((parse('31 March 2020') - parse('01 April 2019')).days) + 'D', axis = 1)
+df['Scheme Year'] = 'year-9'
+#These two things need updating to be automatically updated
+df['Marker'] = df.apply(lambda x: 'not-applicable' if x['Value'] == '' else '', axis = 1)
 
-df = df[['Period', 'Scheme Year', 'Support Element', 'Supplier', 'Marker', 'Value']]
+df = df[['Period', 'Scheme Year', 'Support Element', 'Category', 'Marker', 'Value']]
 
 COLUMNS_TO_NOT_PATHIFY = ['Period', 'Value']
 
@@ -205,7 +160,6 @@ for col in df.columns.values.tolist():
 	except Exception as err:
 		raise Exception('Failed to pathify column "{}".'.format(col)) from err
 
-mapping = {}
 with open("info.json") as f:
     info_json = json.load(f)
 
@@ -213,16 +167,6 @@ with open("info.json") as f:
     info_json["transform"]["columns"]['Value']["measure"] = "http://gss-data.org.uk/def/measure/expenditure"
     info_json["transform"]["columns"]['Value']["datatype"] = "integer"
 
-print("Mapping for: ", 'gbpexpenditure')
-print(json.dumps(mapping, indent=2))
-print("\n")
-
-trace.Supplier("Values taken from 'Supplier' column")
-trace.Support_Element("Values one of {}", dimensions)
-trace.Value("Values taken from {} columns", dimensions)
-trace.Period("Value added as '15 August 2018 to 31 March 2019'")
-trace.Scheme_Year("Value added as 'Year 8'")
-trace.Value("Removed commas and whitespaces from Values")
 
 scraper.dataset.title = 'Warm Home Discount Scheme: Total expenditure by obligated suppliers'
 
@@ -241,35 +185,17 @@ We update this chart on an annual basis.
 df.head(10)
 
 
-# %%
+# In[7]:
 
 
-import os
-from urllib.parse import urljoin
+csvName = 'totalexpenditure'
 
-csvName = 'gbpexpenditure.csv'
-out = Path('out')
-out.mkdir(exist_ok=True)
-df.drop_duplicates().to_csv(out / csvName, index = False)
-df.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
+scraper.dataset.family = 'energy'
 
-scraper.dataset.family = 'edvp'
-dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower() + "/gbpexpenditure"
-scraper.set_base_uri('http://gss-data.org.uk')
-scraper.set_dataset_id(dataset_path)
-
-csvw_transform = CSVWMapping()
-csvw_transform.set_csv(out / csvName)
-csvw_transform.set_mapping(info_json)
-csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
-csvw_transform.write(out / f'{csvName}-metadata.json')
-
-with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
+cubes.add_cube(scraper, df, csvName)
 
 
-# %%
-
+# In[8]:
 
 
 with open('info.json') as f:
@@ -283,27 +209,22 @@ scraper.distributions[0].title = title
 scraper
 
 
-# %%
+# In[9]:
 
 
-link = scraper.distributions[0].downloadURL
-columns = ['Nation', 'Value']
-trace.start(publisher, 'nationexpenditure', columns, link)
-
-df = scraper.distributions[0].as_pandas()
+df = pd.read_csv('percentage-spend.csv')
 
 df = df.rename(columns={'Total Spend' : 'Value'})
-df['Period'] = df.apply(lambda x: 'gregorian-interval/'+ str(parse('15 August 2018').date()) + 'T00:00:00/P' + str((parse('31 March 2019') - parse('15 August 2018')).days) + 'D', axis = 1)
-df['Scheme Year'] = 'year-8'
-df['Value'] = df.apply(lambda x: sanitize_values(x['Value'].replace('%', '')), axis = 1)
+df['Period'] = df.apply(lambda x: 'gregorian-interval/'+ str(parse('01 April 2019').date()) + 'T00:00:00/P' + str((parse('31 March 2020') - parse('01 April 2019')).days) + 'D', axis = 1)
+df['Scheme Year'] = 'year-9'
+#These two things need updating to be automatically updated
 
-df = df.replace({'Nation' : {'England' : 'E92000001',
+df = df.replace({'Category' : {'England' : 'E92000001',
                              'Scotland' : 'S92000003',
                              'Wales' : 'W92000004'}})
 
-df = df[['Period', 'Scheme Year', 'Nation', 'Value']]
+df = df[['Period', 'Scheme Year', 'Category', 'Value']]
 
-mapping = {}
 with open("info.json") as f:
     info_json = json.load(f)
 
@@ -311,14 +232,6 @@ with open("info.json") as f:
     info_json["transform"]["columns"]['Value']["measure"] = "http://gss-data.org.uk/def/measure/expenditure"
     info_json["transform"]["columns"]['Value']["datatype"] = "double"
 
-print("Mapping for: ", 'nationexpenditure')
-print(json.dumps(mapping, indent=2))
-print("\n")
-
-trace.Nation("Values taken from 'Nation' column")
-trace.Nation("Replace Values with Geography Codes")
-
-trace.Value("Values taken from Total spend column")
 
 scraper.dataset.title = 'Warm Home Discount Scheme: Percentage spend by nation'
 
@@ -337,31 +250,18 @@ We update this chart on an annual basis.
 df.head(10)
 
 
-# %%
+# In[10]:
 
 
-import os
-from urllib.parse import urljoin
+csvName = 'nationexpenditure'
 
-csvName = 'nationexpenditure.csv'
-out = Path('out')
-out.mkdir(exist_ok=True)
-df.drop_duplicates().to_csv(out / csvName, index = False)
-df.drop_duplicates().to_csv(out / (csvName + '.gz'), index = False, compression='gzip')
+scraper.dataset.family = 'energy'
 
-scraper.dataset.family = 'edvp'
-dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower() + "/nationexpenditure"
-scraper.set_base_uri('http://gss-data.org.uk')
-scraper.set_dataset_id(dataset_path)
-
-csvw_transform = CSVWMapping()
-csvw_transform.set_csv(out / csvName)
-csvw_transform.set_mapping(info_json)
-csvw_transform.set_dataset_uri(urljoin(scraper._base_uri, f'data/{scraper._dataset_id}'))
-csvw_transform.write(out / f'{csvName}-metadata.json')
-
-with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
-    metadata.write(scraper.generate_trig())
+cubes.add_cube(scraper, df, csvName)
 
 
-# %%
+# In[11]:
+
+
+cubes.output_all()
+
