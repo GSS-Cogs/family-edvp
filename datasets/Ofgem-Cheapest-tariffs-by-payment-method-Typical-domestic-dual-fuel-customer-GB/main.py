3a73f86f4a53081f0dc1d4169d3a1545ff6f1dcd
RedWalters <jonathan.j.walters@Gmail.com>
2021-08-25 14:38:54 +0100

    having to add csv into folder as ofgem scraping isnt currently possible without large changes

--- datasets/Ofgem-Cheapest-tariffs-by-payment-method-Typical-domestic-dual-fuel-customer-GB/main.py
+++ datasets/Ofgem-Cheapest-tariffs-by-payment-method-Typical-domestic-dual-fuel-customer-GB/main.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-# %%
 
-# %%
+# In[29]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# In[30]:
+
 
 import datetime
 import json
@@ -21,7 +28,7 @@ scraper.distributions[0].title = title
 scraper
 
 
-# %%
+# In[31]:
 
 
 def gregorian_day(date):
@@ -39,48 +46,43 @@ def sanitize_values(value):
          return value
 
 
-# %%
-
-trace = TransformTrace()
-link = scraper.distributions[0].downloadURL
-link = link.split('?')[0] # Remove ?fake=.csv which is added dataURL since scraper only supports links ending with a proper file extension
-columns = ['Period', 'Payment Method', 'Value']
-publisher = "The Office of Gas and Electricity Markets"
-trace.start(publisher, title, columns, link)
+# In[32]:
 
 
-# %%
-df = scraper.distributions[0].as_pandas()
+df = pd.read_csv('cheapest-tariffs-by-paym.csv')
 scraper.distributions[0]
+df
 
-# %%
+
+# In[33]:
+
+
 dimensions = list(df.columns) # list of columns
 dimensions = [col for col in dimensions if 'date' not in col.lower()] # list of the dimensions
 print(dimensions)
-trace.Period("Values taken from 'Date' column")
 # Not sure what to call this column, Dimension_1
-trace.Payment_Method("Values one of {}", dimensions)
-trace.Value("Values taken from {} columns", dimensions)
 
-df_new_shape = pd.melt(df, id_vars=["Date"])
-df_final = df_new_shape.rename(columns={"Date": "Period", "variable": "Payment Method", "value": "Value"})
+df_new_shape = pd.melt(df, id_vars=["\n"])
+df_final = df_new_shape.rename(columns={"\n": "Period", "variable": "Payment Method", "value": "Value"})
 
-# %%
-df_final['Period'] = pd.to_datetime(pd.Series(df_final['Period']), format="%d/%m/%Y").astype(str)
+
+# In[34]:
+
+
+df_final['Period'] = pd.to_datetime(pd.Series(df_final['Period']), format="%Y/%m/%d").astype(str)
 #df_final["Period"] = df_final["Period"].apply(gregorian_day)
 df_final['Period'] = 'day/' + df_final['Period'].astype(str)
 
-trace.Period("Formatted time to 'gregorian-day/dd/mm/yyyy'")
 df_final["Value"] = df_final["Value"].apply(sanitize_values)
-trace.Value("Removed commas and whitespaces from Values")
 
-trace.store(title, df_final)
 cubes.add_cube(scraper, df, title)
-trace.render("spec_v1.html")
 
 df_final['Payment Method'] = df_final['Payment Method'].apply(pathify)
 
-# %%
+
+# In[35]:
+
+
 csvName = "{}.csv".format(pathify(title))
 df_final.to_csv("./out/{}".format(csvName), index=False)
 
@@ -88,36 +90,37 @@ out = Path('out')
 out.mkdir(exist_ok=True)
 
 scraper.dataset.comment = """
-This data compares the cheapest available tariffs offered by the large legacy suppliers with the cheapest tariff 
-available in the market by payment method (direct debit, standard credit and prepayment). Figures are based on a 
+This data compares the cheapest available tariffs offered by the large legacy suppliers with the cheapest tariff
+available in the market by payment method (direct debit, standard credit and prepayment). Figures are based on a
 typical domestic dual fuel customer paying by direct debit.
 """
 
 scraper.dataset.description = """
-This data compares the cheapest available tariffs offered by the large legacy suppliers with the cheapest tariff 
-available in the market by payment method (direct debit, standard credit and prepayment). Figures are based on a 
+This data compares the cheapest available tariffs offered by the large legacy suppliers with the cheapest tariff
+available in the market by payment method (direct debit, standard credit and prepayment). Figures are based on a
 typical domestic dual fuel customer paying by direct debit.
 
 Methodology
-We calculate the bill values associated with the different tariff types using a ‘typical medium domestic consumer’. 
-As of April 2020, typical domestic consumption values (TDCV) for a medium consumer are 12,000kWh/year for gas and 
-2,900kWh/year for electricity (profile class 1). The previous typical domestic consumption values (TDCV), that came 
-into effect as of October 2017 were 12,000kWh/year for gas and 3,100kWh/year for electricity (profile class 1). 
+We calculate the bill values associated with the different tariff types using a ‘typical medium domestic consumer’.
+As of April 2020, typical domestic consumption values (TDCV) for a medium consumer are 12,000kWh/year for gas and
+2,900kWh/year for electricity (profile class 1). The previous typical domestic consumption values (TDCV), that came
+into effect as of October 2017 were 12,000kWh/year for gas and 3,100kWh/year for electricity (profile class 1).
 The chart includes collective switching tariffs from Q1 2016.
 
-All tariffs shown in the chart are for a dual fuel customer. Dual fuel refers to a situation where a customer takes 
+All tariffs shown in the chart are for a dual fuel customer. Dual fuel refers to a situation where a customer takes
 gas and electricity from the same supplier.
-Tariffs with limited availability depending on customer features (for example, tariffs which are only available to 
-new customers, also known as ‘acquisition’ tariffs, or tariffs restricted to certain regions) are excluded from the 
+Tariffs with limited availability depending on customer features (for example, tariffs which are only available to
+new customers, also known as ‘acquisition’ tariffs, or tariffs restricted to certain regions) are excluded from the
 calculation to make sure that all tariffs considered are generally available to all customers across GB.
-Tariffs available with white label suppliers are included in the calculation of the cheapest tariff. White label 
-suppliers are organisations without supply licenses that partner with an active licensed supplier to offer gas and 
+Tariffs available with white label suppliers are included in the calculation of the cheapest tariff. White label
+suppliers are organisations without supply licenses that partner with an active licensed supplier to offer gas and
 electricity using their own brand.
 """
 
 
+# In[36]:
 
-# %%
+
 scraper.dataset.family = 'edvp'
 dataset_path = pathify(os.environ.get('JOB_NAME', f'gss_data/{scraper.dataset.family}/' + Path(os.getcwd()).name)).lower()
 scraper.set_base_uri('http://gss-data.org.uk')
@@ -132,17 +135,28 @@ csvw_transform.write(out / f'{csvName}-metadata.json')
 with open(out / f'{csvName}-metadata.trig', 'wb') as metadata:
     metadata.write(scraper.generate_trig())
 
-# %%
+
+# In[37]:
+
+
 df = df_final[df_final['Payment Method'] == "market-direct-debit"]
 df.head(6)
 
-# %%
+
+# In[38]:
+
+
 #scraper.dataset.family = 'edvp'
-#codelistcreation = ['Payment Method'] 
+#codelistcreation = ['Payment Method']
 #df = df_final
 #codeclass = CSVCodelists()
 #for cl in codelistcreation:
 #    if cl in df.columns:
 #        codeclass.create_codelists(pd.DataFrame(df[cl]), 'codelists2', scraper.dataset.family, Path(os.getcwd()).name.lower())
 
-# %%
+
+# In[38]:
+
+
+
+
