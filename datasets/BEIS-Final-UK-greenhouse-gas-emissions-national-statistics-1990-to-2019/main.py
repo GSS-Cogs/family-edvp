ef2e98e6bb60c19e97aa130bcd86d48cec88f395
“osamede20” <osamede.asemota@gsscogs.uk>
2022-02-22 14:40:03 +0000

    revising codes in line with csvcubed tool and formatting

--- datasets/BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2019/main.py
+++ datasets/BEIS-Final-UK-greenhouse-gas-emissions-national-statistics-1990-to-2019/main.py
@@ -1,300 +1,234 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[27]:
-
-
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.2
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-
-# In[28]:
-
-
 import json
-
 import pandas as pd
-
 from gssutils import *
 
-scraper = Scraper(seed = "info.json")
-scraper.distributions = [x for x in scraper.distributions if hasattr(x, "mediaType")]
-scraper
-
-#Add cubes class
-cubes = Cubes("info.json")
-
-#Add TransformTrace
-trace = TransformTrace()
 
 
-# In[29]:
+metadata = Scraper(seed="info.json")
 
 
-distribution  = scraper.distribution(latest=True, title = lambda x:"2019 UK greenhouse gas emissions: final figures - data tables" in x)
-datasetTitle = distribution.title
-distribution
+distribution = metadata.distribution(
+    latest=True,
+    mediaType="application/vnd.oasis.opendocument.spreadsheet",
+    title=lambda x: "UK greenhouse gas emissions: final figures - data tables (alternative ODS format)"
+    in x,
+)
 
-# Extract all the tabs and its content from the spread sheet
+
 tabs = distribution.as_databaker()
+tabs = [
+    tab for tab in tabs if tab.name in ["1_1", "1_2", "1_3", "1_4", "1_5", "1_6", "3_1"]
+]
 
-# List out all the tab names to cross verify with spread sheet
+
+tidied_sheets = []
 for tab in tabs:
-    print(tab.name)
-
-columns = ["Period", "Gas", "dim", "nc_category_child", "nc_sub_sector_parent", "nc_sector_parent", "Unit"]
-
-# Filtering the tabs which are required and start stage-1 transform
-for tab in tabs:
-    if tab.name == "1.1":
-        print(tab.name)
-        trace.start(datasetTitle, tab, columns, distribution.downloadURL)
-        remove = tab.filter(contains_string("Footnotes")).expand(RIGHT).expand(DOWN)
-#         savepreviewhtml(remove, fname=tab.name + "Preview.html")
+    if tab.name == "1_1":
 
         cell = tab.excel_ref("A1")
+        period = cell.shift(0, 5).fill(RIGHT).is_not_blank().is_not_whitespace()
+        gas = cell.shift(0, 5).fill(DOWN).is_not_blank().is_not_whitespace()
+        unit = "Million of tonnes of carbon dioxide equivalent (MtCO2e)"
+        observations = period.waffle(gas)
+        dimensions = [
+            HDim(period, "Period", DIRECTLY, ABOVE),
+            HDim(gas, "Gas", DIRECTLY, LEFT),
+            HDimConst("Unit", unit),
+        ]
 
-        period = cell.shift(0, 2).fill(RIGHT).is_not_blank().is_not_whitespace()
-        trace.Period('Defined from cell B3 right')
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        print(tab.name)
 
-        gas = cell.shift(0, 2).fill(DOWN).is_not_blank().is_not_whitespace()-remove
-        trace.Gas("Defined from cell A4 down")
+    elif tab.name in ["1_2", "1_3", "1_4", "1_5"]:
+        cell = tab.filter("NC Sector")
+        period = cell.shift(RIGHT).fill(RIGHT).is_not_blank().is_not_whitespace()
+        gas = "All"
+        unit = "Million of tonnes of carbon dioxide equivalent (MtCO2e)"
+        stop_cell = tab.filter("Grand Total")
+        nc_category = tab.filter("NC Category").fill(DOWN)
+        nc_sector = nc_category.is_blank().shift(LEFT)
+        nc_sub_sector = nc_category.shift(LEFT).is_not_blank()
 
-        unit = "Million tonnes carbon dioxide equivalent (MtCO2e)"
-        trace.Unit("Hard coded as Million tonnes carbon dioxide equivalent (MtCO2e)")
+        if tab.name == "1_2":
+            gas = "All"
+        elif tab.name == "1_3":
+            gas = "Carbon Dioxide CO2"
+        elif tab.name == "1_4":
+            gas = "Methane CH4"
+        elif tab.name == "1_5":
+            gas = "Nitrous Oxide N2O"
+
+        observations = period.waffle(nc_category).is_not_blank().is_not_whitespace()
+
+        dimensions = [
+            HDim(period, "Period", DIRECTLY, ABOVE),
+            HDim(nc_category, "NC Category", DIRECTLY, LEFT),
+            HDim(nc_sector, "NC Sector", CLOSEST, ABOVE),
+            HDim(nc_sub_sector, "NC Sub Sector", CLOSEST, ABOVE),
+            HDimConst("Gas", gas),
+            HDimConst("Unit", unit),
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        print(tab.name)
+
+    elif tab.name == "1_6":
+        cell = tab.filter("NC Sector")
+        period = cell.shift(RIGHT).fill(RIGHT).is_not_blank().is_not_whitespace()
+        gas = "All"
+        unit = "Million of tonnes of carbon dioxide equivalent (MtCO2e)"
+
+        stop_cell = tab.filter("Grand Total")
+        nc_category = tab.filter("NC Category").fill(DOWN)
+        nc_sector = nc_category.is_blank().shift(LEFT)
+
+        gas = "Fluorinated Gases F Gases"
+
+        observations = period.waffle(nc_category).is_not_blank().is_not_whitespace()
+
+        dimensions = [
+            HDim(period, "Period", DIRECTLY, ABOVE),
+            HDim(nc_category, "NC Category", DIRECTLY, LEFT),
+            HDim(nc_sector, "NC Sector", CLOSEST, ABOVE),
+            HDimConst("Gas", gas),
+            HDimConst("Unit", unit),
+        ]
+
+        tidy_sheet = ConversionSegment(tab, dimensions, observations)
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        print(tab.name)
+
+    if tab.name == "3_1":
+        cell = tab.filter("Geographic coverage")
+
+        period = cell.shift(2).fill(RIGHT).is_not_whitespace()
+        gas = cell.shift(2).fill(DOWN).is_not_whitespace()
+        inclusions = cell.shift(1).fill(DOWN).is_not_whitespace()
+
+        geographic_coverage = cell.fill(DOWN).is_not_whitespace()
 
         observations = period.waffle(gas)
+        unit = "Million of tonnes of carbon dioxide equivalent (MtCO2e)"
 
         dimensions = [
             HDim(period, "Period", DIRECTLY, ABOVE),
             HDim(gas, "Gas", DIRECTLY, LEFT),
-            HDimConst("Unit", "Million tonnes carbon dioxide equivalent (MtCO2e)"),
+            HDim(inclusions, "Inclusions-Exclusions", CLOSEST, ABOVE),
+            HDim(geographic_coverage, "Geographic Coverage", CLOSEST, ABOVE),
+            HDimConst("Unit", unit),
         ]
-
         tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-        trace.with_preview(tidy_sheet)
-        trace.store("combined_dataframe", tidy_sheet.topandas())
+        df = tidy_sheet.topandas()
+        tidied_sheets.append(df)
+        print(tab.name)
 
-tabs = distribution.as_databaker()
 
-columns = ["Period", "Gas", "Nc_Category_Child", "Nc_Sub_Sector_Parent", "Nc_Sector_Parent", "Unit"]
+df = pd.concat(tidied_sheets).fillna("")
 
-# Filtering the tabs which are required and start stage-1 transform
-tabs_i_want = ["1.2", "1.3", "1.4", "1.5", "1.6"]
-tabs = [x for x in tabs if x.name in tabs_i_want]
-for tab in tabs:
-    print(tab.name)
-    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
 
-    remove = tab.filter('Footnotes:').expand(RIGHT).expand(DOWN)
+df.rename(
+    columns={
+        "OBS": "Value",
+        "DATAMARKER": "Marker",
+        "Inclusions-Exclusions": "Breakdown",
+    },
+    inplace=True,
+)
 
-    cell = tab.excel_ref("A1")
 
-    period = cell.shift(1, 2).fill(RIGHT).is_not_blank().is_not_whitespace()
-    trace.Period("Defined from cell C3 right")
+df["Value"] = pd.to_numeric(df["Value"], errors="raise", downcast="float")
+df["Value"] = df["Value"].astype(float).round(3)
+df["Period"] = df["Period"].astype(float).astype(int)
 
-    gas = "All"
-    trace.Gas("Hardcoded as all")
 
-    unit = "Million tonnes carbon dioxide equivalent (MtCO2e)"
-    trace.Unit("Hardcoded as Million tonnes carbon dioxide equivalent (MtCO2e)")
+df["NC Sub Sector"] = df.apply(
+    lambda x: "All" if x["NC Sub Sector"] == x["NC Sector"] else x["NC Sub Sector"],
+    axis=1
+)
 
-    nc_category_child = cell.shift(1, 2).fill(DOWN)
-    trace.Nc_Category_Child("Defined from cell B4 down")
 
-    nc_sub_sector_parent = nc_category_child.shift(LEFT).is_not_blank() - remove
-    trace.Nc_Sub_Sector_Parent("Defined from cell A3 down which is not bold")
+# +
+badInheritance = [
+    "Aviation between UK and Crown Dependencies",
+    "Shipping between UK and Crown Dependencies",
+    "Aviation between the Crown Dependencies and Overseas Territories",
+]
 
-    nc_sector_parent = tab.filter('NC Category').fill(DOWN).is_blank().shift(LEFT) - remove
-    trace.Nc_Sector_Parent("Defined from cell A3 down which is bold")
+df["Breakdown"] = df.apply(
+    lambda x: "" if x["Geographic Coverage"] in badInheritance else x["Breakdown"],
+    axis=1
+)
+df["Breakdown"] = df.apply(
+    lambda x: "None"
+    if "" in x["Breakdown"] and x["Geographic Coverage"] == "United Kingdom"
+    else x["Breakdown"],
+    axis=1
+)
+df["Breakdown"] = df.apply(
+    lambda x: "None" if x["Geographic Coverage"] in badInheritance else x["Breakdown"],
+    axis=1
+)
 
-    if tab.name == '1.2':
-        gas = 'all'
-    elif tab.name == '1.3':
-        gas = 'Carbon Dioxide CO2'
-    elif tab.name == '1.4':
-        gas = 'Methane CH4'
-    elif tab.name == '1.5':
-        gas = 'Nitrous Oxide N2O'
-    elif tab.name == '1.6':
-        gas = 'Fluorinated Gases F Gases'
+df["Measure Type"] = "Gas Emissions"
+df = df.replace(
+    {
+        "Gas": {"Total": "All"},
+        "Geographic Coverage": {"United Kingdom only": "United Kingdom"},
+    }
+)
 
-    observations = period.waffle(nc_category_child).is_not_blank().is_not_whitespace()
+indexNames = df[df["Breakdown"] == "Net emissions/removals from LULUCF"].index
+df.drop(indexNames, inplace=True)
 
-    dimensions = [
-        HDim(period, "Period", DIRECTLY, ABOVE),
-        HDim(nc_category_child, "Nc Category Child", CLOSEST, ABOVE),
-        HDim(nc_sub_sector_parent, "Nc Sub Sector Parent", CLOSEST, ABOVE),
-        HDim(nc_sector_parent, "Nc Sector Parent", CLOSEST, ABOVE),
-        HDimConst("Gas", gas),
-        HDimConst("Unit", "Million tonnes carbon dioxide equivalent (MtCO2e)")
+df["NC Category"] = df["NC Category"].str.replace("/", "-")
+df["Breakdown"] = df["Breakdown"].str.replace("/", "-")
+
+df = df.replace(
+    {
+        "Breakdown": {
+            "excluding-net-emissions-removals-from-land-use-land-use-change-and-forestry-lulucf": "excluding-net-emissions-removals-from-lulucf"
+        }
+    }
+)
+
+# -
+
+df = df.replace(
+    {
+        "Geographic Coverage": {"": "United Kingdom"},
+        "NC Category": {"": "All"},
+        "NC Sub Sector": {"": "All"},
+        "NC Sector": {"": "All"},
+        "Breakdown": {"": "All"},
+    }
+)
+
+
+df["Measure"] = "Gas Emissions"
+
+
+df = df[
+    [
+        "Period",
+        "Geographic Coverage",
+        "NC Sector",
+        "NC Sub Sector",
+        "NC Category",
+        "Gas",
+        "Breakdown",
+        "Value",
     ]
-    tidy_sheet = ConversionSegment(tab, dimensions, observations)
-    savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-    # trace.with_preview(tidy_sheet)
-    trace.store("combined_dataframe", tidy_sheet.topandas())
+]
 
 
-columns = ["Period", "Geographic Coverage", "Inclusions-Exclusions", "Gas", "Year", "Value"]
-
-tabs = distribution.as_databaker()
-for tab in tabs:
-    if tab.name == "3.1":
-
-        trace.start(datasetTitle, tab, columns, distribution.downloadURL)
-
-        remove = tab.filter(contains_string("Footnotes")).expand(RIGHT).expand(DOWN)
-        cell = tab.excel_ref("A1")
-
-        period = cell.shift(2, 2).fill(RIGHT).is_not_whitespace()
-        trace.Period("Defined from cell D3 and right")
-
-        gas = cell.shift(2, 2).fill(DOWN).is_not_whitespace()
-        trace.Gas("Defined from cell C3 down")
-
-        inclusions_exclusions = cell.shift(1, 2).fill(DOWN).is_not_whitespace()
-        trace.Inclusions_Exclusions("Defined from cell B3 down")
-
-        geographic_coverage = cell.shift(0, 2).fill(DOWN).is_not_whitespace()-remove
-        trace.Geographic_Coverage("Defined from cell A3 down")
-
-        observations = period.waffle(gas)-remove
-
-        dimensions =[
-            HDim(period, "Period", DIRECTLY, ABOVE),
-            HDim(gas, "Gas", DIRECTLY, LEFT),
-            HDim(inclusions_exclusions, "Inclusions Exclusions", CLOSEST, ABOVE),
-            HDim(geographic_coverage, "Geographic Coverage", CLOSEST, ABOVE ),
-            HDimConst("Unit", "Million tonnes carbon dioxide equivalent (MtCO2e)")
-        ]
-        tidy_sheet = ConversionSegment(tab, dimensions, observations)
-        savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-        trace.with_preview(tidy_sheet)
-        trace.store("combined_dataframe", tidy_sheet.topandas())
-
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe").fillna('')
-df.rename(columns = {'OBS': 'Value', 'DATAMARKER':'Marker'}, inplace = True)
-
-
-# In[30]:
-
-
-# replace the nans now we've confirmed they're where they should be
-replace_nans = {
-    "Marker": "",
-    "Nc Category Child": "all",
-    "Nc Sub Sector Parent": "all",
-    "Nc Sector Parent": "all",
-    "Inclusions Exclusions": "all",
-    "Geographic Coverage": "United Kingdom"
-}
-for col, na_val in replace_nans.items():
-    df[col][df[col] == ""] = na_val
-
-# confirm there's no blanks remaining where there shouldn't be blanks
-for col in [x for x in df.columns.values if x not in ["Value", "Marker"]]:
-    assert "" not in df[col].unique(), f'Column "{col}" has one or more blank entries and shouldn\'t. Got {df[col].unique()}'
-
-
-# In[31]:
-
-
-def left(s, amount):
-    return s[:amount]
-def date_time (date):
-    if len(date) == 6:
-        return 'year/' + left(date, 4)
-
-df['Period'] =  df["Period"].apply(date_time)
-
-
-# In[32]:
-
-
-pd.set_option('display.float_format', lambda x: '%.5f' % x)
-
-badInheritance = ['Aviation between UK and Crown Dependencies', 'Shipping between UK and Crown Dependencies', 'Aviation between the Crown Dependencies and Overseas Territories']
-
-df['Inclusions Exclusions'] = df.apply(lambda x: '' if x['Geographic Coverage'] in badInheritance else x['Inclusions Exclusions'], axis = 1)
-df['Inclusions Exclusions'] = df.apply(lambda x: 'None' if '' in x['Inclusions Exclusions'] and x['Geographic Coverage'] == 'United Kingdom' else x['Inclusions Exclusions'], axis = 1)
-df['Inclusions Exclusions'] = df.apply(lambda x: 'None' if x['Geographic Coverage'] in badInheritance else x['Inclusions Exclusions'], axis = 1)
-
-df.rename(columns = {'OBS': 'Value', 'DATAMARKER':'Marker', 'Inclusions Exclusions' : 'Breakdown', 'Nc Category Child' : 'Nc Category', 'Nc Sub Sector Parent' : 'Nc Sub Sector', 'Nc Sector Parent' : 'Nc Sector'}, inplace = True)
-
-df['Measure Type'] = 'Gas Emissions'
-df = df.replace({'Unit' : {'Million tonnes carbon dioxide equivalent (MtCO2e)' : 'Millions of Tonnes of CO2 Equivalent'},
-                 'Gas' : {'Total' : 'All'},
-                 'Geographic Coverage' : {'United Kingdom only' : 'United Kingdom'},
-                 'Marker' : {':' : 'not-available'}})
-
-indexNames = df[ df['Breakdown'] == 'Net emissions/removals from LULUCF' ].index
-df.drop(indexNames, inplace = True)
-
-
-# In[33]:
-
-
-COLUMNS_TO_NOT_PATHIFY = ['Period', 'Value', 'Marker']
-
-for col in df.columns.values.tolist():
-    if col in COLUMNS_TO_NOT_PATHIFY:
-        continue
-    try:
-        df[col] = df[col].apply(pathify)
-    except Exception as err:
-        raise Exception('Failed to pathify column "{}".'.format(col)) from err
-
-df['Nc Category'] = df['Nc Category'].str.replace('/', '-')
-df['Breakdown'] = df['Breakdown'].str.replace('/', '-')
-
-df = df.replace({'Breakdown' : {'excluding-net-emissions-removals-from-land-use-land-use-change-and-forestry-lulucf' : 'excluding-net-emissions-removals-from-lulucf'}})
-
-df = df[['Period', 'Geographic Coverage', 'Nc Sector', 'Nc Sub Sector', 'Nc Category', 'Gas', 'Breakdown', 'Value', 'Marker']]
-
-df
-
-
-# In[34]:
-
-
-from IPython.core.display import HTML
-for col in df:
-    if col not in ['Value']:
-        df[col] = df[col].astype('category')
-        display(HTML(f"<h2>{col}</h2>"))
-        display(df[col].cat.categories)
-
-
-# In[35]:
-
-
-scraper.dataset.comment = """Final estimates of UK territorial greenhouse gas emissions.
-In accordance with international reporting and carbon trading protocols, each of these gases are weighted by their global warming potential (GWP), so that total greenhouse gas emissions can be reported on a consistent basis (in carbon dioxide equivalent units)."""
-cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
-cubes.output_all()
-
-trace.render("spec_v1.html")
-
-
-# In[ ]:
-
-
-from IPython.core.display import HTML
-for col in df:
-    if col not in ['Value']:
-        df[col] = df[col].astype('category')
-        display(HTML(f"<h2>{col}</h2>"))
-        display(df[col].cat.categories)
+df.to_csv("observations.csv", index=False)
+catalog_metadata = metadata.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file("catalog-metadata.json")
 
