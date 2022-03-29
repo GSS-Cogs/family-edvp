9aecbfe209884f836a421f06a150ffcd7f76f34f
RedWalters <jonathan.j.walters@Gmail.com>
2022-03-29 14:18:17 +0100

    updating to csvcubed

--- datasets/Ofgem-Cheapest-tariffs-by-payment-method-Typical-domestic-dual-fuel-customer-GB/main.py
+++ datasets/Ofgem-Cheapest-tariffs-by-payment-method-Typical-domestic-dual-fuel-customer-GB/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[65]:
+# In[18]:
 
 
 import datetime
@@ -14,14 +14,12 @@ from gssutils import *
 from urllib.parse import urljoin
 
 scraper = Scraper(seed="info.json")
-# cubes = Cubes("info.json", base_url="http://gss-data.org.uk/data/gss_data/energy/cheapest-tariffs-by-payment-method-typical-domestic-dual-fuel-customer-gb/")
-cubes = Cubes()
 title = "Cheapest tariffs by payment method: Typical domestic dual fuel customer (GB)"
 scraper.distributions[0].title = title
 scraper
 
 
-# In[66]:
+# In[19]:
 
 
 def gregorian_day(date):
@@ -39,7 +37,7 @@ def sanitize_values(value):
          return value
 
 
-# In[67]:
+# In[20]:
 
 
 df = pd.read_csv('cheapest-tariffs-by-paym.csv')
@@ -47,7 +45,7 @@ scraper.distributions[0]
 df
 
 
-# In[68]:
+# In[21]:
 
 
 dimensions = list(df.columns) # list of columns
@@ -59,7 +57,8 @@ df_new_shape = pd.melt(df, id_vars=["\n"])
 df_final = df_new_shape.rename(columns={"\n": "Period", "variable": "Payment Method", "value": "Value"})
 
 
-# In[69]:
+
+# In[22]:
 
 
 df_final['Period'] = pd.to_datetime(pd.Series(df_final['Period']), format="%Y/%m/%d").astype(str)
@@ -69,10 +68,6 @@ df_final['Period'] = 'day/' + df_final['Period'].astype(str)
 df_final["Value"] = df_final["Value"].apply(sanitize_values)
 
 df_final['Payment Method'] = df_final['Payment Method'].apply(pathify)
-
-
-# In[70]:
-
 
 scraper.dataset.comment = """
 This data compares the cheapest available tariffs offered by the large legacy suppliers with the cheapest tariff
@@ -99,20 +94,20 @@ new customers, also known as ‘acquisition’ tariffs, or tariffs restricted to
 calculation to make sure that all tariffs considered are generally available to all customers across GB.
 Tariffs available with white label suppliers are included in the calculation of the cheapest tariff. White label
 suppliers are organisations without supply licenses that partner with an active licensed supplier to offer gas and
-electricity using their own brand.
-"""
+electricity using their own brand."""
 
 
-# In[71]:
+# In[23]:
 
 
 scraper.dataset.family = 'energy'
 
-cubes.add_cube(scraper, df_final, title)
+df_final.to_csv('observations.csv', index=False)
 
 
-# In[72]:
+# In[24]:
 
 
-cubes.output_all()
+catalog_metadata = scraper.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
 
