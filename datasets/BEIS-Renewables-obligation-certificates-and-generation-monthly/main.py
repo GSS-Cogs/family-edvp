9f6087f0ecf7bcadfd18cee58ee5059f45cb4add
RedWalters <jonathan.j.walters@Gmail.com>
2022-05-04 14:58:05 +0100

    actually need to commit changes lmao

--- datasets/BEIS-Renewables-obligation-certificates-and-generation-monthly/main.py
+++ datasets/BEIS-Renewables-obligation-certificates-and-generation-monthly/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
+# In[60]:
 
 
 from gssutils import *
@@ -13,17 +13,7 @@ scraper.distributions = [x for x in scraper.distributions if hasattr(x, "mediaTy
 scraper
 
 
-# In[27]:
-
-
-#Add cubes class
-cubes = Cubes("info.json")
-
-#Add TransformTrace
-trace = TransformTrace()
-
-
-# In[28]:
+# In[61]:
 
 
 # Extract latest distribution  of the required dataset and datasetTitle
@@ -32,7 +22,7 @@ datasetTitle = distribution.title
 distribution
 
 
-# In[29]:
+# In[62]:
 
 
 # Extract all the tabs and its content from the spread sheet
@@ -44,71 +34,45 @@ for tab in tabs:
 
 columns = ["Technology Group", "Generation Type", "Roc Per Mwh", "Period", "Qtr", "Month", "Element"]
 
-# +
+
+# In[63]:
+
+
 # Filtering the tabs which are required and start stage-1 transform
 #tabs_i_want = ["Financial Year", "Quarter", "FY-only sites", "Month"]
 tabs_i_want = ["FY-only sites", "Month"]
 #all data in financial year and quarter tabs are taken from month tab
 
 tabs = [x for x in tabs if x.name in tabs_i_want]
+
+tidy_tabs = []
+
 for tab in tabs:
     print(tab.name)
-
-    trace.start(datasetTitle, tab, columns, distribution.downloadURL)
 
     remove = tab.filter(contains_string("Data are sourced")).expand(RIGHT).expand(DOWN)
     cell = tab.excel_ref("A1")
 
     generation_type = cell.shift(1, 5).fill(DOWN).is_not_blank().is_not_whitespace()
-    trace.Generation_Type("Defined from cell B6 below which is not blank")
 
     roc_per_mwh = cell.shift(2, 5).fill(DOWN)-remove
-    trace.Roc_Per_Mwh("Defined from cell C6 below which is not blank")
 
-    if tab.name == "Quarter":
-        qtr = cell.shift(2, 5).fill(RIGHT).is_not_blank().is_not_whitespace()
-        trace.Qtr("Defined from cell D6 and right which is not blank")
-        period = cell.shift(2, 4).fill(RIGHT).is_not_blank().is_not_whitespace()
-        trace.Period("Defined from cell D5 and right which is not blank")
-    elif tab.name == "Month":
-        month = cell.shift(2, 5).fill(RIGHT).is_not_blank().is_not_whitespace()
-        trace.Month("Defined from cell D6 and right which is not blank")
-        period = cell.shift(2, 4).fill(RIGHT).is_not_blank().is_not_whitespace()
-        trace.Period("Defined from cell D5 and right which is not blank")
-    else:
-        period = cell.shift(2, 5).fill(RIGHT).is_not_blank().is_not_whitespace()
-        trace.Period("Defined from cell D6 and right which is not blank")
+    period = cell.shift(2, 5).fill(RIGHT).is_not_blank().is_not_whitespace()
 
-    element = tab.one_of(["Certificates", "Equivalent Generation"])
-    trace.Element("Defined cell A7, A132, A257 and A275")
-
-    techno_group = cell.shift(0, 5).fill(DOWN).is_not_blank().is_not_whitespace()-element
+    techno_group = cell.shift(0, 5).fill(DOWN).is_not_blank().is_not_whitespace()
 
     technology_group = techno_group - tab.excel_ref("A").filter(contains_string("Summary Technology Group")) - remove
-    trace.Technology_Group("Defined from cell A8 which is not blank")
 
-    observations = cell.shift(3, 6).expand(RIGHT).expand(DOWN).is_not_whitespace() - element.expand(RIGHT) & period.expand(DOWN)
+    observations = cell.shift(3, 6).expand(RIGHT).expand(DOWN).is_not_whitespace()
 
-    if tab.name == "Quarter":
-
-        dimensions = [
-            HDim(technology_group, "Technology Group", CLOSEST, ABOVE),
-            HDim(generation_type, "Generation Type", CLOSEST, ABOVE),
-            HDim(roc_per_mwh, "Roc Per Mwh", DIRECTLY, LEFT, cellvalueoverride = {'' : 'all'}),
-            HDim(period, "Period", CLOSEST, LEFT),
-            HDim(element, "Element", CLOSEST, ABOVE),
-            HDim(qtr, "Qtr", CLOSEST, LEFT)
-        ]
-
-    elif tab.name == "Month":
+    if tab.name == "Month":
 
         dimensions = [
             HDim(technology_group, "Technology Group", CLOSEST, ABOVE),
             HDim(generation_type, "Generation Type", CLOSEST, ABOVE),
             HDim(roc_per_mwh, "Roc Per Mwh", DIRECTLY, LEFT, cellvalueoverride = {'' : 'all'}),
             HDim(period, "Period", CLOSEST, LEFT),
-            HDim(element, "Element", CLOSEST, ABOVE),
-            HDim(month, "Month", CLOSEST, LEFT)
+            HDim(period, "Month", CLOSEST, LEFT)
         ]
     else:
 
@@ -116,31 +80,40 @@ for tab in tabs:
             HDim(technology_group, "Technology Group", CLOSEST, ABOVE),
             HDim(generation_type, "Generation Type", CLOSEST, ABOVE),
             HDim(roc_per_mwh, "Roc Per Mwh", DIRECTLY, LEFT, cellvalueoverride = {'' : 'all'}),
-            HDim(period, "Period", CLOSEST, LEFT),
-            HDim(element, "Element", CLOSEST, ABOVE),
+            HDim(period, "Period", CLOSEST, LEFT)
 
         ]
 
     tidy_sheet = ConversionSegment(tab, dimensions, observations)
     savepreviewhtml(tidy_sheet, fname=tab.name + "Preview.html")
-    trace.with_preview(tidy_sheet)
-    trace.store("combined_dataframe", tidy_sheet.topandas())
+    tidy_tabs.append(tidy_sheet.topandas())
 
 
-# In[30]:
+# In[64]:
 
 
-df = trace.combine_and_trace(datasetTitle, "combined_dataframe").fillna("NaN")
+df = pd.concat(tidy_tabs).fillna("NaN")
 
 def left(s, amount):
     return s[:amount]
 def right(s, amount):
     return s[-amount:]
 
+df.to_csv('test.csv', index=False)
+
+df
+
+
+# In[65]:
+
+
+
 #df['Period'] = df.apply(lambda x: 'quarter/' + left(x['Period'], 4) + '-Q' + left(x['Qtr'], 1) if 'NaN' not in x['Qtr'] else x['Period'], axis =1 )
-df['Month'] = df.apply(lambda x : x['Month'][:-2] if x['Month'][-1] == 'p' else x['Month'], axis = 1)
+df['Month'] = df.apply(lambda x : x['Month'][5:] if x['Month'][:1] == '2' else x['Month'], axis = 1)
+
 
 df = df.replace({'Month' : {'September'  : '09',
+                            ' September'  : '09',
                             'October'    : '10',
                             'November'   : '11',
                             'December'   : '12',
@@ -154,6 +127,9 @@ df = df.replace({'Month' : {'September'  : '09',
                             'August'     : '08'},
                  'Technology Group' : {'Total' : 'All'}})
 
+indexNames = df[ df['Roc Per Mwh'] == 'ROCs per MWh' ].index
+df.drop(indexNames, inplace = True)
+
 df['Period'] = df.apply(lambda x: 'month/' + left(x['Period'], 4) + '-' + x['Month'] if 'NaN' not in x['Month'] else x['Period'], axis =1 )
 
 df['Period'] = df.apply(lambda x: 'government-year/' + left(x['Period'], 4) if 'NaN' in x['Month'] else x['Period'], axis = 1)
@@ -162,43 +138,26 @@ df['Roc Per Mwh'] = df.apply(lambda x: round(float(x['Roc Per Mwh']), 2) if 'all
 
 df = df.rename(columns={'OBS' : 'Value'})
 
-df = df[['Period', 'Technology Group', 'Generation Type', 'Roc Per Mwh', 'Value', 'Element']]
-
-COLUMNS_TO_NOT_PATHIFY = ['Period', 'Value', 'Roc Per Mwh']
-
-for col in df.columns.values.tolist():
-    if col in COLUMNS_TO_NOT_PATHIFY:
-        continue
-    try:
-        df[col] = df[col].apply(pathify)
-    except Exception as err:
-        raise Exception('Failed to pathify column "{}".'.format(col)) from err
-
-indexNames = df[ df['Element'] == 'equivalent-generation' ].index
-df.drop(indexNames, inplace = True)
-
-#equivalent generation is calculated in sheet by dividing the certificates by the ROC per Mwh values
-
-df = df.drop(columns=['Element'])
+df = df[['Period', 'Technology Group', 'Generation Type', 'Roc Per Mwh', 'Value']]
 
 df['Value'] = df.apply(lambda x: int(x['Value']) if '.' in str(x['Value']) else x['Value'], axis = 1)
 
 df
 
 
-# In[31]:
+# In[66]:
 
 
 scraper.dataset.title = 'Renewables obligation: certificates and generation'
 scraper.dataset.comment = 'Data on the UK’s renewables sector. This data relates to certificates and generation associated with the renewables obligation scheme. Published monthly on the second Thursday of each month.'
 
-cubes.add_cube(scraper, df.drop_duplicates(), datasetTitle)
-cubes.output_all()
+df.to_csv('observations.csv', index=False)
 
-trace.render("spec_v1.html")
+catalog_metadata = scraper.as_csvqb_catalog_metadata()
+catalog_metadata.to_json_file('catalog-metadata.json')
 
 
-# In[32]:
+# In[67]:
 
 
 from IPython.core.display import HTML
